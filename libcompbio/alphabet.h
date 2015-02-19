6a81431a5aa89213d60a8dc144e11f6a8b5c3163
Lucas van Dijk <info@return1.net>
2015-02-19 17:31:17 +0100

    Change alphabet API a bit, remove Letter class.

--- libcompbio/alphabet.h
+++ libcompbio/alphabet.h
@@ -2,43 +2,32 @@
 #define COMPBIO_ALPHABET_H
 
 #include <string>
-#include <memory>
-#include <vector>
 #include <functional>
 
 namespace compbio {
 
 using std::string;
-using std::unique_ptr;
-using std::vector;
 using std::function;
-
-class Letter {
-    private:
-        char letter;
-        char complement;
-
-    public:
-        Letter(char letter); // can be used implicitly
-        Letter(char letter, char complement);
-
-        char getLetter() const;
-        void setLetter(char letter);
-
-        char getComplement() const;
-        void setComplement(char complement);
-
-        virtual ~Letter() { }
-};
 
 class Alphabet {
     private:
-        vector<unique_ptr<Letter> > letters;
-        function<int(const Letter&, const Letter&)> cost_function;
+        std::string letters;
+        std::string complements;
+        function<int(char a, char b)> cost_function;
 
     public:
-        Alphabet(const string& letters, const string& complement);
-        Alphabet(const char* letters, const char* complement);
+        Alphabet(const string& letters, const string& complements);
+
+        Alphabet(const string& letters, const string& complements,
+            function<int(char a, char b)>& cost_function);
+
+        // Copying and moving
+        Alphabet(const Alphabet& other);
+        Alphabet(Alphabet&& other);
+
+        char complement(char letter);
+
+        void setCostFunction(function<int(char a, char b)>& cost_function);
 };
 
 }
