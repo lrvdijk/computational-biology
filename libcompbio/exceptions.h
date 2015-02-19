c8bb6904dff88a109410a2d4f73c6daeb73b5dad
Lucas van Dijk <info@return1.net>
2015-02-19 21:27:53 +0100

    Add proper constructors to exceptions

--- libcompbio/exceptions.h
+++ libcompbio/exceptions.h
@@ -2,13 +2,18 @@
 #define COMPBIO_EXCEPTIONS_H
 
 #include <stdexcept>
+#include <string>
 
 class NotImplemented : public std::runtime_error
 {
+    public:
+        NotImplemented(const std::string& message) : std::runtime_error(message) { }
 };
 
 class AlphabetException : public std::runtime_error
 {
+    public:
+        AlphabetException(const std::string& message) : std::runtime_error(message) { }
 };
 
 #endif
