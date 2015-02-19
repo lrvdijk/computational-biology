a8d40f9fce3bed8e972b52ee96a0719d46ad4e41
Lucas van Dijk <info@return1.net>
2015-02-19 16:18:22 +0100

    A bit more code for alphabets

--- libcompbio/alphabet.h
+++ libcompbio/alphabet.h
@@ -1,13 +1,17 @@
 #ifndef COMPBIO_ALPHABET_H
 #define COMPBIO_ALPHABET_H
 
+#include <string>
 #include <memory>
 #include <vector>
+#include <functional>
 
 namespace compbio {
 
+using std::string;
 using std::unique_ptr;
 using std::vector;
+using std::function;
 
 class Letter {
     private:
@@ -30,8 +34,11 @@ class Letter {
 class Alphabet {
     private:
         vector<unique_ptr<Letter> > letters;
+        function<int(const Letter&, const Letter&)> cost_function;
 
     public:
+        Alphabet(const string& letters, const string& complement);
+        Alphabet(const char* letters, const char* complement);
 };
 
 }
