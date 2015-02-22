6e438c80bea70889e54938d267e9f775fb1586d9
Lucas van Dijk <info@return1.net>
2015-02-22 21:17:03 +0100

    Remove cost function from alphabet, will now be part of alignment classes.

--- libcompbio/alphabet.cpp
+++ libcompbio/alphabet.cpp
@@ -9,10 +9,6 @@ using namespace compbio;
 Alphabet::Alphabet(const string& letters, const string& complements) :
                    letters(letters), complements(complements) 
 {
-    this->cost_function = [] (char a, char b) -> int {
-        throw NotImplemented("This alphabet does not have a cost function defined.");
-    };
-
     if(this->letters.size() != this->complements.size())
     {
         throw AlphabetException("The string length for letters and complements must match!.");
@@ -20,20 +16,12 @@ Alphabet::Alphabet(const string& letters, const string& complements) :
 
 }
 
-Alphabet::Alphabet(const string& letters, const string& complements, 
-                   function<int(char a, char b)>& cost_function) :
-                   letters(letters), complements(complements), cost_function(cost_function)
-{
-}
-
-Alphabet::Alphabet(const Alphabet& other) : letters(other.letters), complements(other.complements),
-    cost_function(other.cost_function)
+Alphabet::Alphabet(const Alphabet& other) : letters(other.letters), complements(other.complements)
 {
 }
 
 Alphabet::Alphabet(Alphabet&& other) : letters(std::move(other.letters)), 
-                                       complements(std::move(other.complements)),
-                                       cost_function(std::move(other.cost_function))
+                                       complements(std::move(other.complements))
 {
 }
 
@@ -60,8 +48,3 @@ char Alphabet::complement(char letter) const
     return this->complements[index];
 }    
 
-void Alphabet::setCostFunction(function<int(char a, char b)>& cost_function)
-{
-    this->cost_function = cost_function;
-}
-    
--- libcompbio/alphabet.h
+++ libcompbio/alphabet.h
@@ -14,13 +14,10 @@ class Alphabet {
     private:
         string letters;
         string complements;
-        function<int(char a, char b)> cost_function;
 
     public:
         // Standard constructors
         Alphabet(const string& letters, const string& complements);
-        Alphabet(const string& letters, const string& complements,
-            function<int(char a, char b)>& cost_function);
 
         // Copying and moving
         Alphabet(const Alphabet& other);
@@ -29,7 +26,6 @@ class Alphabet {
         virtual const std::string& getLetters() const;
         virtual const std::string& getComplements() const;
         virtual char complement(char letter) const;
-        void setCostFunction(function<int(char a, char b)>& cost_function);
 
         virtual ~Alphabet() { }
 };
