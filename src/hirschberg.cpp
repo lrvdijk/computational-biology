26701fe512dadbc6a69f27f70f20464290a020f3
Lucas van Dijk <info@return1.net>
2015-02-23 20:14:24 +0100

    Add hirschberg executable

---
+++ src/hirschberg.cpp
@@ -0,0 +1,35 @@
+#include <tuple>
+#include <string>
+#include <iostream>
+#include <libcompbio/alignment/hirschberg.h>
+
+using namespace compbio;
+
+int main()
+{
+    Sequence seq1("AGTACGCA");
+    Sequence seq2("TATGC");
+
+    HirschbergAlignment aligner(seq1, seq2, [] (char a, char b) {
+        if(a == b) {
+            return 2;
+        } else if(a == '-') {
+            return -1;
+        } else if(b == '-') {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+
+    std::string alignment1;
+    std::string alignment2;
+    std::tie(alignment1, alignment2) = aligner.align();
+
+    std::cout << alignment1 << std::endl;
+    std::cout << alignment2 << std::endl;
+    
+    return 0;
+}
+
+
